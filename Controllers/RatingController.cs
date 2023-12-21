c9b65b86208b417e4bad5f9bcde33d72c66dccd1
Isaiah Schmidt <isiahdschmidt@outlook.com>
2023-12-20 20:43:00 -0500

    controllers done

---
+++ Controllers/RatingController.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using RestaurantRaterAPI.Data;
+using RestaurantRaterAPI.ViewModels;
+
+namespace RestaurantRaterAPI.Controllers
+{
+    [Route("[controller]")]
+    public class RatingController : Controller
+    {
+       private readonly RestaurantDbContext _context;
+
+       public RatingController(RestaurantDbContext context)
+       {
+        _context = context;
+       }
+
+       [HttpGet]
+       public async Task<IActionResult> GetRatings()
+       {
+            List<RatingListItem> ratingListItems = await _context.Ratings.Select(r=> 
+                        new RatingListItem
+                        {
+                            Id = r.Id,
+                            RestaurantName = r.Restaurant!.Name,
+                            FoodScore = r.FoodScore,
+                            EnvironmentScore = r.EnvironmentScore,
+                            CleanlinessScore = r.CleanlinessScore
+                        }).ToListAsync();
+
+                        return Ok(ratingListItems);
+       }
+
+       [HttpPost]
+       public async Task<IActionResult> AddRating(RatingCreate model)
+       {
+            if (ModelState.IsValid)
+            {
+                // manual mapping
+                Rating rating = new Rating()
+                {
+                    RestaurantId = model.RestaurantId,
+                    CleanlinessScore = model.CleanlinessScore,
+                    EnvironmentScore = model.EnvironmentScore,
+                    FoodScore = model.FoodScore
+                };
+
+                await _context.Ratings.AddAsync(rating);
+                await _context.SaveChangesAsync();
+
+                return Ok("Rating successfully added");
+            }
+            return BadRequest("Rating could not be added");
+        }
+
+        [HttpPut]
+        [Route("{ratingId:int}")]
+        public async Task<IActionResult> UpdateRating(int ratingId, RatingEdit model)
+        {
+            if (ratingId == model.Id && ModelState.IsValid)
+            {
+                Rating ratingInDb = await _context.Ratings.Include(r=>r.Restaurant).SingleOrDefaultAsync(x=>x.Id == ratingId);
+
+                if (ratingInDb is null)
+                    return NotFound();
+                else
+                {
+                    if(model.RestaurantId > 0)
+                    {
+                        ratingInDb.RestaurantId = model.RestaurantId;
+                        ratingInDb.CleanlinessScore = model.CleanlinessScore;
+                        ratingInDb.EnvironmentScore = model.EnvironmentScore;
+                        ratingInDb.FoodScore = model.FoodScore;
+
+                        await _context.SaveChangesAsync();
+                        return Ok("successfully updated");
+                    } 
+                }
+            }
+            return BadRequest("could not be updated");
+        }
+
+        [HttpDelete]
+        [Route("{ratingId:int}")]
+        public async Task<IActionResult> DeleteRating(int ratingId)
+        {
+             if (ratingId > 0)
+            {
+                Rating ratingInDb = await _context.Ratings.Include(r=>r.Restaurant).SingleOrDefaultAsync(x=>x.Id == ratingId);
+
+                if (ratingInDb is null)
+                    return NotFound();
+                else
+                {
+                    _context.Ratings.Remove(ratingInDb);
+                    await _context.SaveChangesAsync();
+                    return Ok("successfully deleted");
+                }
+            }
+            return BadRequest("could not be deleted");
+        }
+    }
+}
--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -1,11 +1,14 @@
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RestaurantRaterAPI.Data;
+using RestaurantRaterAPI.ViewModels;
 
 namespace RestaurantRaterAPI.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class RestaurantController : ControllerBase
 {
     private readonly RestaurantDbContext _context;
@@ -18,32 +21,109 @@ public class RestaurantController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetRestaurants()
     {
-        List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+        List<Restaurant> restaurants = await _context.Restaurants.Include(r => r.Ratings).ToListAsync();
+
+        //manual mapping -> from: List<Restaurant> to List<RestaurantListItem>
+        List<RestaurantListItem> restaurantListItems = restaurants.Select(r => new RestaurantListItem
+        {
+            Id = r.Id,
+            Name = r.Name,
+            Rating = r.Rating
+        }).ToList();
+
         return Ok(restaurants);
     }
 
-    [HttpGet("{id:int}")]
+    [HttpGet]
+    [Route("{id:int}")]
     public async Task<IActionResult> GetRestaurantById(int id)
     {
-        Restaurant? restaurant = await _context.Restaurants.FindAsync(id);
-        if (restaurant is null)
+        Restaurant? restaurant = await _context.Restaurants.Include(r => r.Ratings).FirstOrDefaultAsync(r => r.Id == id);
+        // manual mapping
+        if (restaurant != null)
         {
-            return NotFound();
+         RestaurantDetail restaurantDetail = new RestaurantDetail()
+            {
+                Id = restaurant.Id,
+                Name = restaurant.Name,
+                Address = restaurant.Address,
+                OverallScore = restaurant.Rating,
+                FoodRating = restaurant.FoodRating,
+                CleanlinessRating = restaurant.CleanlinessRating,
+                EnvironmentRating = restaurant.EnvironmentRating,
+                IsRecommended = restaurant.IsRecommended
+            };
+
+            return Ok(restaurantDetail);
         }
-        return Ok(restaurant);
+        return NotFound();
+
     }
 
     // Async POST method
     [HttpPost]
-    // [Route("PostRestaurant")]
-    public async Task<IActionResult> PostRestaurant([FromBody] Restaurant request)
+    public async Task<IActionResult> AddRestaurant([FromForm] RestaurantCreate model)
     {
         if (ModelState.IsValid)
         {
-            _context.Restaurants.Add(request);
+            // manual mapping
+            Restaurant restaurant = new Restaurant()
+            {
+                Name = model.Name,
+                Address = model.Address
+            };
+
+            // add to database
+            await _context.Restaurants.AddAsync(restaurant);
             await _context.SaveChangesAsync();
-            return Ok();
+
+            return Ok("Restaurant" + restaurant.Name + " was successfully created");
         }
-        return BadRequest(ModelState);
+        return BadRequest("Restaurant failed to add to database");
     }
+
+    [HttpPut]
+    [Route("/update restaurant/{restaurantId:int}")]
+    public async Task<IActionResult> UpdateRestaurant(int restaurantId, RestaurantEdit model)
+    {
+        if (restaurantId == model.Id && ModelState.IsValid)
+        {
+            //find restaurant in db
+            Restaurant restaurantInDb = await _context.Restaurants.FirstOrDefaultAsync(x=>x.Id == restaurantId);
+
+            if (restaurantInDb != null)
+            {
+            //need to update restaurantInDb's data manually
+            restaurantInDb.Name = model.Name;
+            restaurantInDb.Address = model.Address;
+
+            await _context.SaveChangesAsync();
+            return Ok("Restaurant was successfully added");
+            }
+            return NotFound();
+        }
+        return BadRequest("restaurant failed to update");
+    }
+
+    [HttpDelete]
+    [Route("/delete restaurant/{restaurantId:int}")]
+    public async Task<IActionResult> DeleteRestaurant(int restaurantId)
+    {
+        if (restaurantId > 0)
+        {
+            //find restaurant in db
+            Restaurant restaurantInDb = await _context.Restaurants.FirstOrDefaultAsync(x=>x.Id == restaurantId);
+
+            if (restaurantInDb != null)
+            {
+                _context.Restaurants.Remove(restaurantInDb);
+
+                await _context.SaveChangesAsync();
+                return Ok("Restaurant was successfully deleted");
+            }
+            return NotFound();
+        }
+        return BadRequest("restaurant failed to delete");
+    }
+
 }
